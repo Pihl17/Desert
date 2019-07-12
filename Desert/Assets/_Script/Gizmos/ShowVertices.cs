86724b574c99206d82ac775b72bc5269d64e402d
MPihl <mpihl17@student.aau.dk>
2019-07-12 12:45:45 +0200

    a few things before I update to a newer version of unity

---
+++ Desert/Assets/_Script/Gizmos/ShowVertices.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TileGenerationScript))]
+public class ShowVertices : MonoBehaviour
+{
+	void OnDrawGizmos() {
+		Gizmos.DrawSphere(transform.position, 0.5f);
+		if (GetComponent<MeshFilter>() != null) {
+			
+		}
+	}
+}
